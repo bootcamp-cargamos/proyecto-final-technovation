cdc6e2009b2d1785a8be5a35db8ef2116fd95e71
Aguerrerosipac <githubalicia@gmail.com>
2022-02-24 11:41:22 -0600

    Iniciando_carrusel_dinamico

--- js/components/carrousel.js
+++ js/components/carrousel.js
@@ -2,7 +2,11 @@ export default Vue.component ("carrousel", {
     props: [],
     data: function () {
         return {
-
+          slides: [
+            "https://i.pinimg.com/736x/10/2c/f0/102cf0aacbd7725893ad9e6ac8c1782c.jpg",
+            "https://i.pinimg.com/736x/12/88/7b/12887bd3b00d61b8ee85838567d436f2--traditional-clothes-traditional-styles.jpg",
+            "https://c8.alamy.com/compes/cp4xbk/guatemala-mujeres-tejiendo-cp4xbk.jpg"
+           ],
         };
     },
     computed: {
@@ -14,21 +18,8 @@ export default Vue.component ("carrousel", {
 
     },
     template: `
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="/img/OG-110+Patzun.jpg" class="d-block w-100 height1" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="/img/45148766_289501191664476_4049768493539655680_n.jpg" class="d-block w-100 height1" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="/img/44992007_288940898387172_2825189849055625216_n.jpg" class="d-block w-100 height1" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="/img/guatemala-mujeres-tejiendo-cp4xbk.jpg" class="d-block w-100 height1" alt="...">
-      </div>
+    <div class="slider">
+    <div v-for="(image, index) in slides" :key="index"></div>
     </div>
-  </div>
     `
 });
