561278d03287ee7d86ef38f900a229d27dfe9f8e
ruminadeleon <ruminadeleon19@gmail.com>
2022-02-26 02:57:59 -0600

    en-proceso

--- js/pages/products_page.js
+++ js/pages/products_page.js
@@ -11,7 +11,18 @@ export default Vue.component(
             const nombre= this.$route.params.id
         },
         template: `
-        <div></div>
+        <a class="nav-link dropdown-toggle text-second" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+        aria-expanded="false">
+        Productos
+      </a>
+      <ul class="dropdown-menu b-nav" aria-labelledby="navbarDropdown">
+        <li><a class="dropdown-item" href="/#/categories/zapatos">Zapatos</a></li>
+        <li><a class="dropdown-item" href="/#/categories/camino">Caminos de mesa</a></li>
+        <li><a class="dropdown-item" href="/#/categories/servilletas">Servilletas y manteles</a></li>
+        <li><a class="dropdown-item" href="/#/categories/telas">Telas tipicas</a></li>
+        <li><a class="dropdown-item" href="/#/categories/blusas">Blusas y faldas</a></li>
+      </ul>
+    </li>
         `
 },
 )
